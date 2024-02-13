04f0a3001d38a146a8d4a9a692e206dbed39ba2f
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-02-13 18:22:19 +0100

    Ajout d'une fonction qui permet de calculer les bénefices de l'ensemble des combinaisons d'une liste selectionnées. La fonction récupère au fur et à mesure le bénéfice le plus haut, à la fin elle retourne le bénéfice le plus haut et la combinaison associée.

--- bruteforce.py
+++ bruteforce.py
@@ -59,9 +59,47 @@ def create_news_combinations_lists(l_combinations, l_data_csv):
     return news_combinations_lists
 
 
+def calculate_profit(combinations_lists, l_data_csv):
+    """ Fonction qui permet de calculer les bénéfices de l'ensemble des combinaisons d'une liste selectionnée.
+        La fonction récupère au fur et à mesure le bénéfice le plus haut, à la fin elle retourne le bénefice le plus
+        haut et la combinaison associée.
+    
+    :param: combinations_lists
+    :param: l_data_csv
+    :return: [best_combination, [total_profit]]
+    """
+    total_profit = 0
+    best_combination = []
+    next_combi = 0
+    for single_combi in combinations_lists:
+        temp_profit = 0
+        next_combi += 1
+        for single_action_number in single_combi:
+            single_action_number -= 1
+            select_action = l_data_csv[single_action_number]
+            select_cost_action = int(select_action['CoutParAction'])
+            select_percentage_action = float(select_action['PourcentageBenefice'])
+            result_profit = select_cost_action * select_percentage_action
+            if next_combi == 1:
+                total_profit += result_profit
+            else:
+                temp_profit += result_profit
+
+        if next_combi == 1:
+            best_combination = single_combi
+        else:
+            if temp_profit > total_profit:
+                total_profit = temp_profit
+
+                best_combination = single_combi
+    return [best_combination, [total_profit]]
+
+
 list_numbers = list(range(1, 21))
 list_data_csv = get_data_csv()
 news_lists_combinations = generate_combinations(list_numbers)
 n_combinations_lists = create_news_combinations_lists(news_lists_combinations, list_data_csv)
-print(n_combinations_lists)
+final_result = calculate_profit(n_combinations_lists, list_data_csv)
+print(final_result)
+
 
