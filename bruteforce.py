371e990063bddddac5b8e498772a415df49fc24e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-02-13 15:18:58 +0100

    Création d'une variable list_number qui permet d'avoir une liste de nombre de 1 à 20. Création de la fonction generate_combinations qui permet de créer toutes les combinaison possible de 1 à 20. La fonction créer plusieurs listes de combinaisons, une liste de combinaison doit contenir au minimum 4 combinaisons, pas de maximum.

--- bruteforce.py
+++ bruteforce.py
@@ -16,5 +16,27 @@ def get_data_csv():
         return list_actions
 
 
+def generate_combinations(l_numbers):
+    """ Fonction qui permet de créer toutes les combinaisons possibles de 1 à 20.
+        Créer plusieurs listes contenant au minimum 4 combinaison
+    :param l_numbers:
+    :return: list_combinations
+    """
+    n = len(l_numbers)
+    list_combinations = []
+    for i in range(1 << n):
+        temp_combination_list = []
+        for j in range(n):
+            if (i >> j) & 1:
+                temp_combination_list.append(l_numbers[j])
+        if temp_combination_list:
+            if len(temp_combination_list) >= 4:
+                list_combinations.append(temp_combination_list)
+    return list_combinations
+
+
+list_numbers = list(range(1, 21))
 list_data_csv = get_data_csv()
+list_combination = generate_combinations(list_numbers)
+
 
