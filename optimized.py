0df1de0334639060987e31d742ee22040bb5fa36
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-02-18 17:01:56 +0100

    Petite modification dans la boucle for ligne 29 afin que le programme puisse commencer à créer la suite de combinaisons en partant d'une combinaison à 1 chiffre permettant ainsi la suppression d'une condition, condition qui permettait d'ignorer l'enregistrement d'un tuple vide.

--- optimized.py
+++ optimized.py
@@ -26,42 +26,41 @@ def generate_combinations(inf_data_csv):
     best_combinations = []
     length = len(inf_data_csv)
     list_data1 = list(range(0, length))
-    for ll in list(range(0, length)):  # indice de 0 à (20 exclus)
+    for ll in list(range(1, length)):  # indice de 0 à (20 exclus)
         list_data2 = combinations(list_data1, ll)
         for i in list_data2:
-            if i != ():
-                select_tuple_i_left = i[:1]
-                select_tuple_i_left = select_tuple_i_left[0]
-                info_i = len(i)
+            select_tuple_i_left = i[:1]
+            select_tuple_i_left = select_tuple_i_left[0]
+            info_i = len(i)
 
-                if info_i == 1:  # si info_i contient qu'une seule valeur
-                    select_action = inf_data_csv[select_tuple_i_left]
+            if info_i == 1:  # si info_i contient qu'une seule valeur
+                select_action = inf_data_csv[select_tuple_i_left]
+                select_purchase_action = int(select_action['CoutParAction'])
+                select_profit = float(select_action['PourcentageBenefice'])
+                if select_purchase_action <= MAX:
+                    calculate_profit = select_purchase_action * select_profit
+                    if calculate_profit > total_profit:
+                        best_combinations = select_action
+                        total_profit = calculate_profit
+                        total_purchase_action += select_purchase_action
+            else:
+                temp_total_profit = 0
+                temp_total_purchase_action = 0
+                temp_combinations = []
+                for combination in i:
+                    select_action = inf_data_csv[combination]
                     select_purchase_action = int(select_action['CoutParAction'])
+                    temp_total_purchase_action += select_purchase_action
                     select_profit = float(select_action['PourcentageBenefice'])
-                    if select_purchase_action <= MAX:
-                        calculate_profit = select_purchase_action * select_profit
-                        if calculate_profit > total_profit:
-                            best_combinations = select_action
-                            total_profit = calculate_profit
-                            total_purchase_action += select_purchase_action
-                else:
-                    temp_total_profit = 0
-                    temp_total_purchase_action = 0
-                    temp_combinations = []
-                    for combination in i:
-                        select_action = inf_data_csv[combination]
-                        select_purchase_action = int(select_action['CoutParAction'])
-                        temp_total_purchase_action += select_purchase_action
-                        select_profit = float(select_action['PourcentageBenefice'])
-                        calculate_profit = select_purchase_action * select_profit
-                        temp_total_profit += calculate_profit
-                        temp_combinations.append(select_action)
+                    calculate_profit = select_purchase_action * select_profit
+                    temp_total_profit += calculate_profit
+                    temp_combinations.append(select_action)
 
-                    if temp_total_purchase_action < MAX:
-                        if temp_total_profit > total_profit:
-                            best_combinations = temp_combinations
-                            total_profit = temp_total_profit
-                            total_purchase_action = temp_total_purchase_action
+                if temp_total_purchase_action < MAX:
+                    if temp_total_profit > total_profit:
+                        best_combinations = temp_combinations
+                        total_profit = temp_total_profit
+                        total_purchase_action = temp_total_purchase_action
     return [best_combinations, total_profit, total_purchase_action]
 
 
