55f5ac27767ad7915dacab3d0298e22acabf691e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-03-29 18:03:09 +0100

    Ajout de la fonction delete_identical_number, fonction qui permet de comparer des valeurs identiques et garde la valeur ayant le meilleur bénéfice.

--- bruteforce_v2.py
+++ bruteforce_v2.py
@@ -54,7 +54,33 @@ def sort_list_data(data3):
     return list_sort
 
 
+def delete_identical_number(data4):
+    """
+    5)
+    Fonction qui compare les valeurs en double pour le champ price et
+    garde la valeur la plus haute
+    """
+    new_list = []
+    for data1 in data4:
+        if new_list:
+            info_new_list = new_list[-1]
+            result_price_new_list = int(float(info_new_list['price']) * 10)
+            result_price_modify1 = int(float(data1['price']) * 10)
+            if result_price_new_list == result_price_modify1:
+                result_profit_new_list = float(info_new_list['profit'])
+                result_profit_modify1 = float(data1['profit'])
+                if result_profit_modify1 > result_profit_new_list:
+                    del new_list[-1]
+                    new_list.append(data1)
+            else:
+                new_list.append(data1)
+        else:
+            new_list.append(data1)
+    return new_list
+
+
 list_csv1 = get_data_csv()
 list_csv2 = modify_list_csv(list_csv1)
 list_csv3 = add_value_csv(list_csv2)
 list_csv4 = sort_list_data(list_csv3)
+list_csv5 = delete_identical_number(list_csv4)
