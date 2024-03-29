d7f4550cdac044dc81446a7b0b0d1889e4271c8d
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-03-29 10:41:32 +0100

    Ajout de la fonction modify_list_csv, fonction qui permet de modifier les données du fichier csv et de garder les valeurs de price superieur à 1.

--- optimized_v1_action1.py
+++ optimized_v1_action1.py
@@ -17,3 +17,15 @@ def get_data_csv():
         list_actions = list(reader)
         return list_actions
 
+
+def modify_list_csv(data1):
+    """
+    2)
+    Fonction qui garde les valeurs de price superieur à 1.
+    """
+    new_list = [c for c in data1 if float(c['price']) > 1]
+    return new_list
+
+
+list_csv1 = get_data_csv()
+list_csv2 = modify_list_csv(list_csv1)
