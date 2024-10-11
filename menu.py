e1106acc7522dfe9eb4e6d330e0ff2f18527ae6e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-10-11 15:03:52 +0200

    Ajout de la fonction select_csv_file qui permet d'afficher les fichiers csv disponible dans le répertoire Data et demande à l'utilisateur quel fichier doit il analyser. Divers modifications concernant l'affichage des messages d'erreur.

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
-from bruteforce_v2 import get_input_data, check_information_entered
+import os
+
+project_root = os.getcwd()
 
 
 def show_menu():
@@ -16,7 +18,8 @@ def show_menu():
         if x:
             user_response = int(user_response)
             if user_response == 1:
-                print("Choix 1 ok")
+                selected_file = select_csv_file()
+                print("Fichier selectionné = ", selected_file)
             elif user_response == 2:
                 print("Choix 2 ok")
             elif user_response == 3:
@@ -24,13 +27,49 @@ def show_menu():
             elif user_response == 4:
                 print("Choix 4 ok")
             else:
-                error_message()
+                error_message(info_message="Error_Menu")
         else:
-            error_message()
+            error_message(info_message="Error_Menu")
 
 
-def error_message():
-    return print("Erreur ! Vous devez faire un choix entre la valeur 1, 2, 3 ou 4.")
+def select_csv_file():
+    select_file = None
+    path = f"{project_root}/Data/"
+    info_file = os.listdir(path)
+    loop = True
+    while loop:
+
+        if info_file:
+            print("-------Liste des fichiers à analizer-------")
+            i = 0
+            for inf_file in info_file:
+                i += 1
+                print(f"{i}) {inf_file}")
+            response = input("Quel fichier souhaitez-vous analizer ?")
+            x = response.isdigit()
+            if x:
+                response = int(response)
+                if response != 0:
+                    if response <= i:
+                        response -= 1
+                        select_file = info_file[response]
+                        loop = False
+                    else:
+                        error_message(info_message="Error_Menu")
+                else:
+                    error_message(info_message="Error_Menu")
+            else:
+                error_message(info_message="Error_Menu")
+        else:
+            error_message(info_message="Error_No_File", info_path=path)
+    return select_file
+
+
+def error_message(info_message=None, info_path=None):
+    if info_message == "Error_Menu":
+        print("Erreur ! Vous devez faire un choix entre les valeurs disponible dans le menu.")
+    elif info_message == "Error_No_File":
+        print(f"Erreur ! Aucun fichier csv n'est présent à l'emplacement suivant : {info_path}")
 
 
 show_menu()
