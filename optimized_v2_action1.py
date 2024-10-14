4ef46e1218e16d19235762ad32b70c47d2a4d3da
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-10-14 14:50:10 +0200

    Ajout de la fonction start_optimized_v2_function qui permet d'exécuter l'ensemble des autres fonction. Supressions de plusieurs fonctions qui ont été déplacé dans un autre fichier prévu. Quelques modifications au niveau des noms de plusieurs variables.

--- optimized_v2_action1.py
+++ optimized_v2_action1.py
@@ -1,113 +1,13 @@
-import csv
-import os
-import time
 from numba import jit
-import sys
-
-path = os.getcwd()
-start_time = time.time()
-INPUT_DATA_CSV = "input_data.csv"
 
 
-def get_input_data():
-    csv_path = os.path.join(path, INPUT_DATA_CSV)
-    with open(csv_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        info_data = list(reader)
-        return info_data
-
-
-def get_data_csv(csv_file):
-    """
-    Fonction qui permet d'extraire les données du fichier Actions.csv.
-    Function that extracts data from the Actions.csv file.
-    """
-    try:
-        control_price_max = int(csv_file[0]['PriceMax'])
-    except ValueError:
-        print("Erreur constatée dans le champ PriceMax, veuillez saisir ")
-        print("une valeur numérique dans le fichier input_data.csv")
-        sys.exit()
-    file_name = csv_file[0]['CsvFileName']
-    csv_path = os.path.join(path, file_name)
-    control_file = os.path.exists(csv_path)
-    if control_file:
-        with open(csv_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            list_actions = list(reader)
-            return list_actions
-    else:
-        print(f"Nom du fichier renseigné dans input_data.csv = {file_name}")
-        print("Le fichier à analyser n'existe pas dans le répertoire Projet7_V2, veuillez")
-        print("contrôler votre saisie dans input_data.csv")
-        sys.exit()
-
-
-def modify_list_csv(data1):
-    """
-    2)
-    Fonction qui garde les valeurs de price supérieures à 1.
-    Function that keeps values of price greater than 1.
-    """
-    new_list = [c for c in data1 if float(c['price']) > 1]
-    return new_list
-
-
-def add_value_csv(data2):
-    """
-    3)
-    Fonction qui calcule et ajoute des valeurs dans le champ total_bénéfice.
-    Function that calculates and adds values to the total_profit field.
-    """
-    for data_csv in data2:
-
-        value_price = float(data_csv['price'])
-        pourcentage = float(data_csv['profit'])
-        data_csv['total_benefice'] = value_price * (pourcentage / 100)
-
-    return data2
-
-
-def sort_list_data(data3):
-    """
-    4)
-    Fonction qui trie la liste.
-    Function that sorts the list.
-    """
-    list_sort = sorted(data3, key=lambda x: float(x['price']), reverse=True)
-
-    return list_sort
-
-
-def delete_identical_number(data4):
-    """
-    5)
-    Fonction qui compare les valeurs en double pour le champ price et garde la valeur la
-    plus haute de profit.
-    Function that compares duplicate values for the price field and keeps the highest
-    profit value.
-    """
-    new_list = []
-    for data1 in data4:
-        if new_list:
-            info_new_list = new_list[-1]
-            result_price_new_list = int(float(info_new_list['price']) * 10)
-            result_price_modify1 = int(float(data1['price']) * 10)
-            if result_price_new_list == result_price_modify1:
-                result_profit_new_list = float(info_new_list['profit'])
-                result_profit_modify1 = float(data1['profit'])
-                if result_profit_modify1 > result_profit_new_list:
-                    del new_list[-1]
-                    new_list.append(data1)
-            else:
-                new_list.append(data1)
-        else:
-            new_list.append(data1)
-    return new_list
+def start_optimized_v2_functions(list_csv5=None, info_price_max=None):
+    result = search_show_result(list_csv5, info_price_max)
+    return result
 
 
 @jit
-def create_modify_table(pm, n, price, tt_benefice):
+def create_modify_table(pm, n, price, sum_result_profit):
     """
     Fonction qui permet de créer un tableau à deux dimensions, les données sont ensuite
     calculées et placées dans le tableau selon leur valeur.
@@ -117,29 +17,27 @@ def create_modify_table(pm, n, price, tt_benefice):
     t = [[0 for x in range(pm + 1)] for x in range(n + 1)]
     for i in range(n + 1):
         for p in range(pm + 1):
-            # if i == 0 or p == 0:
-            #     t[i][p] = 0
             if price[i - 1] <= p:
-                t[i][p] = max(tt_benefice[i - 1] + t[i - 1][p - price[i - 1]], t[i - 1][p])
+                t[i][p] = max(sum_result_profit[i - 1] + t[i - 1][p - price[i - 1]], t[i - 1][p])
             else:
                 t[i][p] = t[i - 1][p]
     return t
 
 
-def search_show_result(data5, data_0):
+def search_show_result(data5, p_max):
     """
     Fonction qui recherches les actions selon les informations dans le tableau puis affiche
     le résultat.
     Function that searches for stocks based on information in the array and displays
     the result.
     """
-    price_max = data_0[0]['PriceMax']
+    price_max = p_max
     pm = int(price_max) * 100
     n = len(data5)
     price = [int(float(c['price']) * 100) for c in data5]
-    tt_benefice = [float(c['total_benefice']) for c in data5]
+    result_profit = [float(c['result_profit']) for c in data5]
 
-    t = create_modify_table(pm, n, price, tt_benefice)
+    t = create_modify_table(pm, n, price, result_profit)
     selected_actions = []
     p = pm
     i = n
@@ -148,25 +46,6 @@ def search_show_result(data5, data_0):
             selected_actions.append(data5[i - 1])
             p -= price[i - 1]
         i -= 1
-    # Afficher les résultats
-    cost = sum([float(a['price']) for a in selected_actions])
-    benefit = sum([float(a['total_benefice']) for a in selected_actions])
-    print("Coût d'achat:", cost)
-    print("Bénéfice:", benefit)
-    print("Liste des actions:")
-    for a in selected_actions:
-        print(f"{a['name']}: Coût: {a['price']} : Pourcentage: {a['profit']} : bénéfices: {a['total_benefice']}")
-
-
-data0 = get_input_data()
-list_csv1 = get_data_csv(data0)
-list_csv2 = modify_list_csv(list_csv1)
-list_csv3 = add_value_csv(list_csv2)
-list_csv4 = sort_list_data(list_csv3)
-list_csv5 = delete_identical_number(list_csv4)
-search_show_result(list_csv5, data0)
-
-
-end_time = time.time()
-result = end_time - start_time
-print(f"Temps d'exécution : {result} seconde(s)")
+    sum_price = sum([float(a['price']) for a in selected_actions])
+    sum_result_profit = sum([float(a['result_profit']) for a in selected_actions])
+    return [selected_actions, sum_price, sum_result_profit]
