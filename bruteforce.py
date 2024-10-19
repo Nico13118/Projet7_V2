2f91df6f790d9f118f30247e3e36eea35e74a117
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-10-19 16:16:09 +0200

    Configuration de la fonction generate_combination qui permet de générer toutes les combinaisons avec la bibliothèque itertools et garde la liste d'actions qui rapporte le meilleur bénéfice.

--- bruteforce.py
+++ bruteforce.py
@@ -1,32 +1,29 @@
 from itertools import combinations
 
 
-def start_bruteforce_functions(list_csv5, info_price_max):
-    final_result = generate_combination(list_csv5, info_price_max)
-    return final_result
-
-
 def generate_combination(data_csv, p_max):
     p_max = int(p_max)
     n_data = len(data_csv)
     final_list = []
     total_price = 0
     total_result_profit = 0
+    number_combinations = 0
     for i in list(range(1, n_data)):
         result_comb = combinations(data_csv, i)
-        for result in result_comb:
-            if len(result) == 1:
-                result = result[0]
-                action_price, action_result_profit = float(result['price']), float(result['result_profit'])
+        for r_comb in result_comb:
+            number_combinations += 1
+            if len(r_comb) == 1:
+                r_comb = r_comb[0]
+                action_price = float(r_comb['price'])
                 if action_price <= p_max:
+                    action_result_profit = float(r_comb['result_profit'])
                     if action_result_profit > total_result_profit:
-                        final_list, action_price, total_result_profit = [result], total_price, action_result_profit
+                        final_list, action_price, total_result_profit = [r_comb], total_price, action_result_profit
             else:
-                action_price = [float(c['price']) for c in result]
+                action_price = [float(c['price']) for c in r_comb]
                 if sum(action_price) <= p_max:
-                    action_result_profit = [c['result_profit'] for c in result]
+                    action_result_profit = [c['result_profit'] for c in r_comb]
                     if sum(action_result_profit) > total_result_profit:
-                        final_list = [c for c in result]
-                        total_price = sum(action_price)
-                        total_result_profit = sum(action_result_profit)
-    return [final_list, total_price, total_result_profit]
+                        final_list = [c for c in r_comb]
+                        total_price, total_result_profit = sum(action_price), sum(action_result_profit)
+    return [final_list, total_price, total_result_profit, number_combinations]
