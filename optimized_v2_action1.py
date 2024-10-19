3a4060879ac665f34c28ab4271a74c90e8db6cc2
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-10-19 17:51:12 +0200

    Ajout de la variable number_combinations qui permet de connaitre le nombre de combinaisons analysées pour trouver le bon résultat

--- optimized_v2_action1.py
+++ optimized_v2_action1.py
@@ -16,20 +16,23 @@ def create_modify_table(pm, n, price, sum_result_profit):
     the array according to their value.
     """
     t = np.zeros((n + 1, pm + 1))
+    number_combinations = 0
     for i in range(n + 1):
+        number_combinations += 1
         for p in range(pm + 1):
+            number_combinations += 1
             if price[i - 1] <= p:
                 t[i][p] = max(sum_result_profit[i - 1] + t[i - 1][p - price[i - 1]], t[i - 1][p])
             else:
                 t[i][p] = t[i - 1][p]
-    return t
+    return t, number_combinations
 
 
 def search_show_result(data5, p_max):
     """
-    Fonction qui recherches les actions selon les informations dans le tableau puis affiche
+    Fonction qui recherches les actions selon les informations dans le tableau puis retourne
     le résultat.
-    Function that searches for stocks based on information in the array and displays
+    Function that searches for stocks based on information in the array and return
     the result.
     """
     price_max = p_max
@@ -38,7 +41,9 @@ def search_show_result(data5, p_max):
     price = [int(float(c['price']) * 100) for c in data5]
     result_profit = [float(c['result_profit']) for c in data5]
 
-    t = create_modify_table(pm, n, price, result_profit)
+    info_result = create_modify_table(pm, n, price, result_profit)
+    t, number_combinations = info_result[0], info_result[1]
+
     selected_actions = []
     p = pm
     i = n
@@ -49,4 +54,4 @@ def search_show_result(data5, p_max):
         i -= 1
     sum_price = sum([float(a['price']) for a in selected_actions])
     sum_result_profit = sum([float(a['result_profit']) for a in selected_actions])
-    return [selected_actions, sum_price, sum_result_profit]
+    return [selected_actions, sum_price, sum_result_profit, number_combinations]
