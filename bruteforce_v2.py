d38c45a7e40f3a0cf9baa8f67085803c99817363
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-10-21 10:29:43 +0200

    Mise à jour du fichier.

--- bruteforce_v2.py
+++ bruteforce_v2.py
@@ -15,7 +15,6 @@ def search_best_profit(full_list_actions, p_max):
     p_max = int(p_max)
     total_price_list = []
     total_result_profit = 0
-
     for n1 in full_list_actions:
         """
         Cette boucle récupère la première actions.
@@ -24,16 +23,11 @@ def search_best_profit(full_list_actions, p_max):
          
         """
         price_n1, result_profit_n1 = float(n1['price']), float(n1['result_profit'])
-
-        temp_total_price_list = []
-        temp_total_result_profit_list = []
-
+        temp_total_price_list, temp_total_result_profit_list = [], []
         temp_total_price_list.append(price_n1)
         temp_total_result_profit_list.append(result_profit_n1)
-
         for n2 in full_list_actions:
             price_n2, result_profit_n2 = float(n2['price']), float(n2['result_profit'])
-
             if price_n2 != price_n1:
                 """
                 Ajout de price_n2 dans temp_total_price_list
@@ -45,19 +39,16 @@ def search_best_profit(full_list_actions, p_max):
                 Calculer la somme temp_total_price_list.
                 """
                 total_sum_price_list = sum(temp_total_price_list)
-
                 if total_sum_price_list > p_max:
                     """ 
                     Si le total est superieur au prix max, on retire les dernières valeurs ajouté dans les deux listes   
                     """
                     temp_total_price_list.pop(-1)
                     temp_total_result_profit_list.pop(-1)
-
         """
         Calculer la somme de temp_total_result_profit_list
         """
         total_sum_result_profit_list = sum(temp_total_result_profit_list)
-
         if total_sum_result_profit_list > total_result_profit:
             """
             Si total_sum_result_profit_list est superieur à total_result_profit, on ajoute :
@@ -65,7 +56,6 @@ def search_best_profit(full_list_actions, p_max):
             total_sum_result_profit_list dans total_result_profit
             """
             total_price_list, total_result_profit = temp_total_price_list, total_sum_result_profit_list
-
     final_action_list = [c for c in full_list_actions if float(c['price']) in total_price_list]
     total_sum_price_list = sum(total_price_list)
     return [final_action_list, total_sum_price_list, total_result_profit]
